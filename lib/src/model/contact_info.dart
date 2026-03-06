af7933ac355b293b026be788aa9efccedb7484fc
suywho <suy.nepal@gmail.com>
2026-03-06 19:45:34 +0545

    feat: implement `ContactInfo` base class
    `ContactInfo` acts as the base class for all Info Classes (`Email`, `Address`, and `Phone`)

---
+++ lib/src/model/contact_info.dart
@@ -0,0 +1,11 @@
+import 'package:contact_book_v2/src/model/label.dart';
+
+class ContactInfo {
+  Label label;
+
+  ContactInfo({required this.label});
+
+  String? formValidator() {
+    return null;
+  }
+}
